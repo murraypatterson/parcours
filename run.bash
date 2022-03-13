dcd613476f89f445ffa18338b4aaa18e700dfb05
Murray Patterson <murray.patterson@gmail.com>
2022-03-13 01:40:32 -0500

    set up some tests

--- run.bash
+++ run.bash
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-python3 test.py tree.nh "0 1" "2:0->1 2:1->0"
+python3 test.py tree.nh "0 1" "2:0->1 2:1->0" > test.out 2> test.log
+
+time=/usr/bin/time
+
+for dir in example felidae
+do
+    cd $dir \
+	&& $time -vo $dir.time \
+		 ../parcours -f config.csv > $dir.out 2> $dir.log \
+	&& cd ..
+done
